4dbb3c5f7a211b257ae749fee06244f8dc0da9a6
farhan-mazumder <farhanmazumder75@gmail.com>
2024-05-11 23:15:19 +0600

    Footer section has been added with responsiveness.

--- src/Layout/MainLayout/MainLayout.jsx
+++ src/Layout/MainLayout/MainLayout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../../Pages/Shared/Navbar/Footer/Footer";
+import Navbar from "../../Pages/Shared/Navbar/Navbar";
 
 const MainLayout = () => {
   return (
     <>
+      <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 };
--- src/Pages/Home/HomePage/HomePage.jsx
+++ src/Pages/Home/HomePage/HomePage.jsx
@@ -1,4 +1,3 @@
-import Navbar from "../../Shared/Navbar/Navbar";
 import bannerBg from "../../../assets/home-1-slide-background.jpg";
 import Carousel from "../Carousel/Carousel";
 import BookATable from "../BookATable/BookATable";
@@ -6,7 +5,6 @@ import BookATable from "../BookATable/BookATable";
 const HomePage = () => {
   return (
     <section>
-      <Navbar />
       <section
         style={{ backgroundImage: `url(${bannerBg})` }}
         className="relative py-16 bg-center bg-no-repeat bg-cover md:py-20 lg:py-28"
