48823a4c19449576218b6c242bba7a111cbd6d5f
farhan-mazumder <farhanmazumder75@gmail.com>
2024-05-13 02:29:01 +0600

    The navbar has been modified.

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -4,9 +4,12 @@ import { GrClose } from "react-icons/gr";
 import { Link, NavLink } from "react-router-dom";
 // import logo from "../../../assets/logo-light.png";
 import logo from "../../../assets/logo-normal.png";
+import useAuth from "../../../Hooks/useAuth";
+import { IoExitOutline } from "react-icons/io5";
 
 const Navbar = () => {
-  const user = false;
+  const { user } = useAuth();
+
   const navItems = (
     <>
       {/* <NavLink
@@ -102,51 +105,49 @@ const Navbar = () => {
                   {!user && navItems}
                 </ul>
 
-                <div
-                  className={`${
-                    user ? "" : "hidden"
-                  } flex items-center mt-4 lg:mt-0`}
-                >
-                  {/* <button
-                    className="hidden mx-4 text-gray-600 transition-colors duration-300 transform lg:block dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 focus:text-gray-700 dark:focus:text-gray-400 focus:outline-none"
-                    aria-label="show notifications"
-                  >
-                    <svg
-                      className="w-6 h-6"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
-                  </button> */}
-
-                  <button
-                    type="button"
-                    className="flex items-center focus:outline-none"
-                    aria-label="toggle profile dropdown"
-                  >
+                {user && (
+                  <>
                     <div
-                      className={`overflow-hidden border-2 border-gray-400 rounded-full w-9 h-9`}
+                      className={`flex items-center mt-4 lg:mt-0 lg:tooltip lg:tooltip-bottom z-20 dropdown dropdown-bottom dropdown-end`}
+                      data-tip={user?.displayName}
                     >
-                      <img
-                        src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
-                        className="object-cover w-full h-full"
-                        alt="avatar"
-                      />
+                      <button
+                        type="button"
+                        className="flex items-center focus:outline-none"
+                        aria-label="toggle profile dropdown"
+                      >
+                        <div
+                          className={`overflow-hidden border-2 border-gray-400 rounded-full w-9 h-9`}
+                          // data-tip={user?.displayName}
+                        >
+                          <div tabIndex={0} role="button" className="">
+                            <img
+                              src={user?.photoURL}
+                              className="object-cover w-full h-full"
+                              alt="avatar"
+                            />
+                          </div>
+                        </div>
+                        <ul
+                          tabIndex={0}
+                          className="dropdown-content z-[3] menu px-3 py-4 shadow bg-base-100 rounded-box w-52 text-white hidden lg:block"
+                        >
+                          <li>
+                            <p className="btn-base btn-secondary px-5 py-2.5 cursor-pointer">
+                              Logout <IoExitOutline />
+                            </p>
+                          </li>
+                        </ul>
+                        <h3 className="mx-2 text-gray-700 dark:text-gray-200 lg:hidden">
+                          {user?.displayName}
+                        </h3>
+                      </button>
                     </div>
-
-                    <h3 className="mx-2 text-gray-700 dark:text-gray-200 lg:hidden">
-                      Khatab wedaa
-                    </h3>
-                  </button>
-                </div>
+                    <button className="btn-base btn-secondary px-5 py-2.5 lg:hidden mt-4">
+                      Logout
+                    </button>
+                  </>
+                )}
               </div>
             </div>
           </div>
