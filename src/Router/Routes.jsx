2bb1b2b93c62638d9104dd7c842bbd8a5552d868
farhan-mazumder <farhanmazumder75@gmail.com>
2024-05-13 00:47:57 +0600

    Transform the testimonial section into a parallax effect.

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout/MainLayout";
 import HomePage from "../Pages/Home/HomePage/HomePage";
+import Login from "../Pages/Login/Login";
+import Registation from "../Pages/Registation/Registation";
 
 const router = createBrowserRouter([
   {
