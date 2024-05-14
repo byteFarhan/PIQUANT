034a198b8cc04ab276a337a649b82fb3a858291d
farhan-mazumder <farhanmazumder75@gmail.com>
2024-05-14 15:17:54 +0600

    The functionality for adding new food items has been implemented.

--- src/Pages/AddFood/AddFood.jsx
+++ src/Pages/AddFood/AddFood.jsx
@@ -1,8 +1,74 @@
 import PageTitle from "../Shared/PageTitle/PageTitle";
 import sectionBg from "../../assets/pattern-2.png";
 import InputForm from "../Shared/InputForm/InputForm";
-
+import { useForm } from "react-hook-form";
+import useAuth from "../../Hooks/useAuth";
+import useCurrentDateFormatted from "../../Hooks/useCurrentDateFormatted";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import toast from "react-hot-toast";
 const AddFood = () => {
+  const { user } = useAuth();
+  const currentDate = useCurrentDateFormatted();
+  const hookFormItems = useForm();
+  const axiosSecure = useAxiosSecure();
+  //   console.log(hookFormItems);
+  const { reset } = hookFormItems;
+
+  const handleAddFood = (data) => {
+    // console.log(data);
+    const {
+      foodName,
+      foodImage,
+      foodCategory,
+      availableQuantity,
+      price,
+      foodOrigin,
+      foodQuantity,
+      rating,
+      description,
+    } = data;
+    const food = {
+      foodName,
+      foodImage,
+      foodCategory,
+      availableQuantity,
+      price,
+      rating,
+      foodOrigin,
+      description,
+      madeBy: user?.displayName,
+      postedDate: currentDate,
+      author: {
+        authorName: user?.displayName,
+        authorEmail: user?.email,
+        authorImg: user?.photoURL,
+      },
+      numberOfPurchases: 0,
+      foodQuantity: foodQuantity,
+      tags: ["Ice Cream", "Dessert", "Sweet"],
+      ingredients: ["Milk", "Cream", "Sugar", "Nougat Pieces"],
+      makingProcedure:
+        "Blend the milk, cream, and sugar until smooth. Fold in the nougat pieces gently. Freeze until set.",
+      reacts: {
+        thumbsUp: 95,
+        heart: 25,
+        laugh: 8,
+      },
+    };
+    console.log(food);
+    axiosSecure
+      .post("/foods", food)
+      .then((res) => {
+        // console.log(res.data);
+        if (res.data.acknowledged) {
+          toast.success("The food has been added successfully.");
+          reset();
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <section>
       <PageTitle pageTitle={"Add A Food"} />
