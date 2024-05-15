2078799831f0ef6de4ded5e792fafef53cb0bbd3
farhan-mazumder <farhanmazumder75@gmail.com>
2024-05-15 20:45:28 +0600

    Added functionality to load foods added by the logged-in user.

--- src/Pages/MyFoods/MyFoods.jsx
+++ src/Pages/MyFoods/MyFoods.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+import useAuth from "../../Hooks/useAuth";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import sectionBg from "../../assets/pattern-2.png";
+
 import PageTitle from "../Shared/PageTitle/PageTitle";
 
 const MyFoods = () => {
+  const axiosSecure = useAxiosSecure();
+  const { user } = useAuth();
+  const [myFoods, setMyFoods] = useState([]);
+  useEffect(() => {
+    axiosSecure
+      .get(`/foods?authorEmail=${user?.email}`)
+      .then((res) => {
+        setMyFoods(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [axiosSecure, user]);
+  console.log(myFoods);
   return (
     <section>
       <PageTitle pageTitle={`Your Added Foods`} />
@@ -9,7 +27,9 @@ const MyFoods = () => {
         style={{ backgroundImage: `url(${sectionBg})` }}
         className="px-5 py-16 md:py-20 md:px-8 lg:px-0 lg:py-32"
       >
-        <section className="mx-auto max-w-7xl"></section>
+        <section className="mx-auto max-w-7xl">
+          <div className=""></div>
+        </section>
       </section>
     </section>
   );
