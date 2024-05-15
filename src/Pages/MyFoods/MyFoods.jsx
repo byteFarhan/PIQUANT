81d8a3cc9a92adcee2c5e47a8e56c48fb701aacd
farhan-mazumder <farhanmazumder75@gmail.com>
2024-05-15 21:03:12 +0600

    Add a table for showcaseing user added foods.

--- src/Pages/MyFoods/MyFoods.jsx
+++ src/Pages/MyFoods/MyFoods.jsx
@@ -4,6 +4,7 @@ import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import sectionBg from "../../assets/pattern-2.png";
 
 import PageTitle from "../Shared/PageTitle/PageTitle";
+import TableB from "./TableB/TableB";
 
 const MyFoods = () => {
   const axiosSecure = useAxiosSecure();
@@ -28,7 +29,9 @@ const MyFoods = () => {
         className="px-5 py-16 md:py-20 md:px-8 lg:px-0 lg:py-32"
       >
         <section className="mx-auto max-w-7xl">
-          <div className=""></div>
+          <div className="">
+            <TableB />
+          </div>
         </section>
       </section>
     </section>
---
+++ src/Pages/MyFoods/TableB/TableB.jsx
@@ -0,0 +1,71 @@
+const TableB = ({ setFoods, foods }) => {
+  return (
+    <>
+      <section className="container px-4 mx-auto">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <h2 className="text-lg font-medium text-gray-800 dark:text-white">
+            Your Added Foods
+          </h2>
+        </div>
+
+        <div className="flex flex-col mt-6">
+          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                  <thead className="bg-gray-50 dark:bg-gray-800">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                      >
+                        <div className="flex items-center gap-x-3">
+                          <span>The Food</span>
+                        </div>
+                      </th>
+
+                      <th
+                        scope="col"
+                        className="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                      >
+                        Food Category
+                      </th>
+
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                      >
+                        Date uploaded
+                      </th>
+
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                      >
+                        Price
+                      </th>
+
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                      >
+                        Food Origin
+                      </th>
+
+                      <th scope="col" className="relative py-3.5 px-4">
+                        <span className="sr-only">Edit</span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900"></tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default TableB;
